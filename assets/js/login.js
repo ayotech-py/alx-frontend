7d1c4eb706c9aaee73e957047179bcf807ab89df
ayotech-py <ajaomalik2018@gmail.com>
2023-02-15 22:00:24 +0100

    frontend

---
+++ assets/js/login.js
@@ -0,0 +1,25 @@
+async function loginIn() {
+    let password = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+    let response = await fetch("http://127.0.0.1:8000/letsquiz_api/login/", {
+        method: "POST",
+        body: JSON.stringify({
+            "username": username,
+            "password": password,
+        }),
+    })
+    let data = await response.json();
+    if (response.status === 400) {
+        console.log(data.error);
+        document.getElementById("response").innerHTML = data.error;
+    } else if (response.status === 200) {
+        console.log(data)
+    } else {
+        console.log(data.error)
+        document.getElementById("response").innerHTML = data.error;
+    }
+}
+
+document.getElementById("login_in").onclick = function() {
+    loginIn();
+}
