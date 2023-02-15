17992dfe0151c5ca902a80721721be07de0e1269
ayotech-py <ajaomalik2018@gmail.com>
2023-02-15 19:12:41 +0100

    frontend

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,5 +1,6 @@
 document.getElementById("sign_up").onclick = function() {
-    let password_01 = document.getElementById("confirm_password").value;
+    signIn()
+/*     let password_01 = document.getElementById("confirm_password").value;
     let password_02 = document.getElementById("password").value;
     let username = document.getElementById("username").value;
     let email = document.getElementById("email").value
@@ -27,5 +28,26 @@ document.getElementById("sign_up").onclick = function() {
         }
     } else {
         console.log("All fields are required!")
+    } */
+}
+
+async function signIn(){
+    let password_01 = document.getElementById("confirm_password").value;
+    let password_02 = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value
+
+    if (password_01 != '' && password_02 != '' && username != '' && email != '') {
+        if (password_01 === password_02) {
+            let response = await fetch("http://127.0.0.1:8000/letsquiz_api/register/", {
+                method: "POST",
+                body: JSON.stringify({
+                    "username": username,
+                    "email": email,
+                    "password": password_01,
+                }, null),
+            })
+            console.log(response)
+        }
     }
 }
