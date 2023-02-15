f86051d4793baa5d41f7282f312c520b12c83de6
ayotech-py <ajaomalik2018@gmail.com>
2023-02-15 20:54:16 +0100

    frontend

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,34 +1,5 @@
 document.getElementById("sign_up").onclick = function() {
-    signIn()
-/*     let password_01 = document.getElementById("confirm_password").value;
-    let password_02 = document.getElementById("password").value;
-    let username = document.getElementById("username").value;
-    let email = document.getElementById("email").value
-
-    if (password_01 != '' && password_02 != '' && username != '' && email != '') {
-        if (password_01 === password_02) {
-            fetch("http://127.0.0.1:8000/letsquiz_api/register/", {
-                method: "POST",
-                body: JSON.stringify({
-                    "username": username,
-                    "email": email,
-                    "password": password_01,
-                }, null),
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("this block of code is running")
-            })
-            .catch((error)  => {
-                document.getElementById("response").innerHTML = error;
-                //console.log(error)
-            });
-        } else {
-            console.log("Password mismatch")
-        }
-    } else {
-        console.log("All fields are required!")
-    } */
+    signIn();
 }
 
 async function signIn(){
@@ -50,9 +21,11 @@ async function signIn(){
             let data = await response.json();
             if (response.status === 400) {
                 console.log(data.error);
+                document.getElementById("response").innerHTML = data.error
             } else if (response.status === 200) {
-                console.log(data)
                 console.log(data.success)
+            } else {
+                console.log(data.error)
             }
         }
     }
