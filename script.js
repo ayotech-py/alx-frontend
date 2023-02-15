35d0dd688f7d1ae71326d2606eb9798065d383b7
ayotech-py <ajaomalik2018@gmail.com>
2023-02-15 23:32:08 +0100

    frontend

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ async function getData() {
     let response = await fetch("http://127.0.0.1:8000/letsquiz_api/getdata", {
         headers: {
             'Authorization': 'Bearer ' + token,
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
         }
     });
     console.log(response.headers)
