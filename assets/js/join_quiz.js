5ec8978c6de77c5eb524e58278a9293dafbf7e60
ayotech-py <ajaomalik2018@gmail.com>
2023-03-05 19:16:23 +0100

    frontend fix

--- assets/js/join_quiz.js
+++ assets/js/join_quiz.js
@@ -28,6 +28,6 @@ document.getElementById("quiz-btn").onclick = function() {
     } else if (window.navigator.connection.rrt <= 400 ) {
         joinQuiz();
     } else {
-        document.getElementById("error").innerHTML = `${window.navigator.connection.effectiveType.endsWith("4g")} | ${window.navigator.connection.effectiveType} | ${window.navigator.connection.rrt}`;
+        document.getElementById("error").innerHTML = `${window.navigator.connection.effectiveType.endsWith("4g")} | ${window.navigator.connection.effectiveType} | ${window.navigator.connection.rtt}`;
     }
 }
