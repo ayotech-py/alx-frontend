b0ccf6d9f0701f57a3553de1711b3310a696b347
ayotech-py <ajaomalik2018@gmail.com>
2023-03-05 17:39:41 +0100

    frontend fix

--- assets/js/join_quiz.js
+++ assets/js/join_quiz.js
@@ -25,7 +25,7 @@ document.getElementById("quiz-btn").onclick = function() {
     console.log(window.navigator.connection.effectiveType.endsWith("4g"))
     if (window.navigator.connection.effectiveType === "4g") {
         joinQuiz();
-    } else if (window.navigator.connection.rrt <= 360 ) {
+    } else if (window.navigator.connection.rrt <= 800 ) {
         joinQuiz();
     } else {
         document.getElementById("error").innerHTML = "Your internet connect is poor or unstable please try again";
