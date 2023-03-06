dedf0673f80d0bf867db0013060e7a4199cb1355
ayotech-py <ajaomalik2018@gmail.com>
2023-03-06 12:14:59 +0100

    frontend

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -45,5 +45,29 @@ async function quizStart(param_1, param_2) {
     }
 }
 
+jQuery(document).ready(function($) {
+
+    if (window.history && window.history.pushState) {
+
+      $(window).on('popstate', function() {
+        var hashLocation = location.hash;
+        var hashSplit = hashLocation.split("#!/");
+        var hashName = hashSplit[1];
+
+        if (hashName !== '') {
+          var hash = window.location.hash;
+          if (hash === '') {
+            alert('Back button was pressed.');
+              window.location='../html/join_quiz_login.html';
+              return false;
+          }
+        }
+      });
+
+      window.history.pushState('forward', null, './#forward');
+    }
+
+  });
+
 scoreBoard();
 quizStart(false, true);
