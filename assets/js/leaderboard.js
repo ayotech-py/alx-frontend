af124869ddd92ae864cd49d0737cfc851ddca2d1
ayotech-py <ajaomalik2018@gmail.com>
2023-03-06 12:57:00 +0100

    frontend

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -46,30 +46,16 @@ async function quizStart(param_1, param_2) {
 }
 
 //This jQuery redirect user back to the join quiz page when back button is clicked
-jQuery(document).ready(function($) {
+window.history.pushState({page: 1}, "", "");
 
-    if (window.history && window.history.pushState) {
-
-      $(window).on('popstate', function() {
-        var hashLocation = location.hash;
-        var hashSplit = hashLocation.split("#!/");
-        var hashName = hashSplit[1];
-
-        if (hashName !== '') {
-          var hash = window.location.hash;
-          if (hash === '') {
-            alert('Back button was pressed.');
-              window.location='../html/join_quiz_login.html';
-              return false;
-          }
-        }
-      });
-
-      window.history.pushState('forward', null, '../html/join_quiz_login.html');
+window.onpopstate = function(event) {
+    if(event){
+        window.location = '../html/join_quiz_login';
+        // Code to handle back button or prevent from navigation
     }
+}
 
-  });
-  console.log("script updated")
+console.log("script updated v2")
 
 scoreBoard();
 quizStart(false, true);
