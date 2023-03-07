bd69bc5b033da50d1c4960836b70dc7986346430
ayotech-py <ajaomalik2018@gmail.com>
2023-03-07 10:30:42 +0100

    change

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -53,6 +53,6 @@ console.log("script updated v2")
 scoreBoard();
 quizStart(false, true);
 
-if (window.performance && window.performance.navigation.type == window.performance.navigation.TYPE_RESERVED) {
+if (window.performance && window.performance.navigation.type == window.performance.navigation.TYPE_RESERVE) {
     window.location = '../html/join_quiz_login.html';
 }
