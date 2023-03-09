4e06871ce78fb76017f0efa50838b787aacd0f06
ayotech-py <102487837+ayotech-py@users.noreply.github.com>
2023-03-09 18:43:52 +0100

    End quiz fix

--- assets/js/end_quiz.js
+++ assets/js/end_quiz.js
@@ -4,4 +4,4 @@ document.getElementById("score").innerHTML = `${score}/40`
 
 document.getElementById("correctAnswer").innerHTML = `Correct Answers: ${score}`;
 
-document.getElementById("wrongAnswer").innerHTML = `Wrong Answers: ${20 - score}`;
+document.getElementById("wrongAnswer").innerHTML = `Wrong Answers: ${40 - score}`;
